84f6519e04c5d9ed9be50b6b5acc8814eca7b7ce
Bushhraa <123033650+Bushhraa@users.noreply.github.com>
2023-02-05 16:43:49 +0600

    Add files via upload

---
+++ Java Class Work/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Box b1=new Box();
+        b1.height=5;
+        b1.weight=10;
+        b1.length=2;
+        b1.calculateArea();
+        System.out.println(" ");
+        Employee e1=new Employee();
+        e1.name="Anika";
+        e1.salary=40000;
+        e1.display();
+    }
+}
