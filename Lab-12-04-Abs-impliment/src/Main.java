cca780e22e2e94de1bbcd9b7f32bca04abb01f2d
Bushhraa <123033650+Bushhraa@users.noreply.github.com>
2023-04-17 16:29:12 +0600

    Add files via upload

---
+++ Lab-12-04-Abs-impliment/src/Main.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Car obj=new Car();
+        Boat obj2 =new Boat();
+        obj.move();
+        obj2.move();
+        obj.carry();
+        Circle c1=new Circle();
+        c1.draw();
+        Line l1=new Line();
+        l1.draw();
+        DemoClass myobj =new DemoClass();
+        myobj.method1();
+        myobj.method2();
+    }
+}
